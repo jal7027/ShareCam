4716d27e5f794dd5b23b3b3419d889857f88fd25
jal7027 <jal7027@gmail.com>
2016-07-20 15:44:59 -0700

    MVC setup

---
+++ ShareCam/ShareCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareCam
+//
+//  Created by Joseph Leiber on 7/20/16.
+//  Copyright © 2016 Reduct Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
